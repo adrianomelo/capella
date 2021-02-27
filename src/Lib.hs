b762c4a7e33903ba6d6a5d439de65e813b639636
Adriano Melo <github@adrianomelo.com>
2021-02-27 20:12:26 +0100

    Create TrackingEvent

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,13 +11,7 @@ import qualified Data.ByteString.Lex.Integral         as Lex
 import           Network.HTTP.Types                   (hContentType, status200,
                                                        status201, status400,
                                                        status404, status405)
-import           Network.Wai                          (Application, Request,
-                                                       Response, pathInfo,
-                                                       queryString,
-                                                       rawPathInfo,
-                                                       responseFile,
-                                                       requestMethod,
-                                                       responseBuilder)
+import           Network.Wai 
 import           Network.Wai.Handler.Warp             (run)
 import           Network.Wai.Middleware.Autohead      (autohead)
 import           Network.Wai.Middleware.RequestLogger (logStdout)
@@ -33,12 +27,27 @@ import System.IO
 import qualified Data.ByteString.Lazy.UTF8  as BLU
 import qualified Data.Binary.Builder as Builder
 import qualified Data.Binary
+import qualified Data.UUID as UUID
+import qualified Data.Text as Text
+import qualified Data.Maybe as Maybe
+import qualified Data.Either as Either
+import Network.Socket (SockAddr)
 
 -- Type synonyms for nicer signatures below
 type Name = Text
 type Age = Int
 type PeopleMap = Map Name Age
 type PeopleVar = TVar PeopleMap
+
+data TrackingEvent = TrackingEvent
+  { uuid :: Text
+  , remote :: SockAddr
+  , referer :: Maybe ByteString
+  , userAgent :: Maybe ByteString
+  } deriving (Show) --, Generic)
+
+--instance FromJSON TrackingEvent
+--instance ToJSON TrackingEvent
 
 -- Common error responses
 notFound :: Response
@@ -47,13 +56,37 @@ notFound = responseBuilder status404 [] "Not found"
 badRequest :: Response
 badRequest = responseBuilder status405 [] "Bad request method"
 
+parsePath :: Text -> Maybe UUID.UUID
+parsePath path =
+  UUID.fromText (Text.take 36 path)
+
+buildEvent :: Request -> UUID.UUID -> TrackingEvent
+buildEvent req uid =
+  TrackingEvent uuid remote referer userAgent
+  where
+    uuid = UUID.toText uid
+    remote = remoteHost req
+    referer = requestHeaderReferer req
+    userAgent = requestHeaderUserAgent req
+
 simpleImageApp :: PeopleVar -> Application
 simpleImageApp var req send = do
-  response <-
+  result <-
     case pathInfo req of
-      [path] -> pure $ responseFile status200 [("Cache-Control", "no-cache")] "small.gif" Nothing
+      [path] ->
+        case parsePath path of
+          Just uuid -> pure $ Either.Right $ buildEvent req uuid
+          _ -> pure $ Either.Left "Incorrect file name"
+      _ -> pure $ Either.Left "Invalid path"
 
-      _ -> pure badRequest
+  print result
+
+  response <-
+    case result of
+      Either.Right event ->
+        pure $ responseFile status200 [("Cache-Control", "no-cache")] "small.gif" Nothing
+      Either.Left message ->
+        pure badRequest
 
   send response
 
